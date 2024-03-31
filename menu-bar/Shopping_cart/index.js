52ed8d4abead0f06acc5190d16d2c5eefa7a2fab
ThinhVinhPhat <120767551+ThinhVinhPhat@users.noreply.github.com>
2024-03-31 15:48:34 +0700

    shopping-cart included

---
+++ menu-bar/Shopping_cart/index.js
@@ -0,0 +1,8 @@
+const removeEL = document.getElementById("remove");
+const includeEL = document.getElementById("include");
+const numberEl = document.getElementById("number");
+
+removeEL.addEventListener("click", ()=>{
+    const amount = 1;
+    
+})
